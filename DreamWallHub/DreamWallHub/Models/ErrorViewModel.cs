755c59a324eccf9432a7bba4295b8b14174c0696
strikeitbg <strikeitbg@gmail.com>
2022-03-17 18:30:20 +0200

    Fresh MVC 6.0

---
+++ DreamWallHub/DreamWallHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DreamWallHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
