e8fd75534407f69b2e906b4863857a72734f34b3
strikeitbg <strikeitbg@gmail.com>
2022-04-30 10:05:56 +0300

    Fixed Layot Admin Area and some security

--- DreamWallHub/DreamWallHub.Core/ViewModels/UserEditViewModel.cs
+++ DreamWallHub/DreamWallHub.Core/ViewModels/UserEditViewModel.cs
@@ -7,15 +7,15 @@ namespace DreamWallHub.Core.ViewModels
         public string UserId { get; set; }
 
         [Required]
-        [StringLength(20, MinimumLength = 6, ErrorMessage = "{0} must be at least {2} and at max {1} characters long.")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "{0} must be at least {2} and at max {1} characters long.")]
         [Display(Name = "First Name")]
         public string? FirstName { get; set; }
         [Required]
-        [StringLength(20, MinimumLength = 6, ErrorMessage = "{0} must be at least {2} and at max {1} characters long.")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "{0} must be at least {2} and at max {1} characters long.")]
         [Display(Name = "Last Name")]
         public string? LastName { get; set; }
         [Required]
-        [StringLength(20, MinimumLength = 6, ErrorMessage = "{0} must be at least {2} and at max {1} characters long.")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "{0} must be at least {2} and at max {1} characters long.")]
         [Display(Name = "UserName")]
         public string? UserName { get; set; }
     }
--- DreamWallHub/DreamWallHub/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ DreamWallHub/DreamWallHub/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -66,17 +66,17 @@ namespace DreamWallHub.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
             [Display(Name = "First Name")]
             public string FirstName { get; set; }
 
             [Required]
-            [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
             [Display(Name = "Last Name")]
             public string LastName { get; set; }
 
             [Required]
-            [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
             [Display(Name = "UserName")]
             public string UserName { get; set; }
             /// <summary>
